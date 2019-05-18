540c8f8bf4ee4f34200e3b0894dc03509da292cd
Paul Mustière <pmustiere@palantir.com>
2019-05-18 16:03:21 +0200

    Working slideshow server

---
+++ server.py
@@ -0,0 +1,65 @@
+import json
+import os
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def available_slideshow_files(self):
+        return os.listdir('./slideshow/')
+
+    def available_pictures(self):
+        files = os.listdir('./pictures/')
+        return [f for f in files if f.endswith('.JPG')]
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path == '/pictures.json':
+            self.handle_list()
+        else:
+            maybe_filename = self.path[1:]
+            if self.path == '/':
+                maybe_filename = 'index.html'
+
+            print(maybe_filename)
+            if maybe_filename in self.available_slideshow_files():
+                self.handle_slideshow(maybe_filename)
+            elif maybe_filename.startswith('pictures/'):
+                self.handle_picture(maybe_filename)
+            else:
+                self.handle_other()
+
+    def handle_list(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(str.encode(json.dumps(self.available_pictures())))
+
+    def handle_slideshow(self, filename):
+        self.send_response(200)
+        self.end_headers()
+        with open('./slideshow/{}'.format(filename), 'rb') as f:
+            content = f.read()
+            self.wfile.write(content)
+
+    def handle_picture(self, filename):
+        self.send_response(200)
+        self.send_header('Content-type', 'image/jpg')
+        self.end_headers()
+        with open('./{}'.format(filename), 'rb') as f:
+            content = f.read()
+            self.wfile.write(content)
+
+    def handle_other(self):
+        self.send_response(500)
+        self.end_headers()
+
+def run(server_class=ThreadingHTTPServer, handler_class=MyHandler):
+    server_address = ('', 80)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
---
+++ slideshow/slideshow.js
@@ -0,0 +1,136 @@
+var MAX_SLIDES = 10;
+var rangeStart = 0;
+var currentSlideInRange = 0;
+var pictures = []
+
+function addSlide(n) {
+  // We're creating this guy
+  // <div class="mySlides fade">
+  // <!-- <div class="numbertext">1 / 3</div> -->
+  // <img src="../pictures/GOPR4805.jpg" style="width:100%">
+  // <div class="text">1 / 3</div>
+  // </div>
+
+  var element = document.createElement('div');
+  element.id = 'slide-' + n;
+  element.className = 'mySlides'; // fade';
+
+  var textBlock = document.createElement('div');
+  textBlock.className = 'text';
+  textBlock.appendChild(document.createTextNode((n+1) + ' / ' + pictures.length));
+  element.appendChild(textBlock);
+
+  var imgBlock = document.createElement('img');
+  imgBlock.style = 'width: 100%;';
+  imgBlock.src = 'http://localhost/pictures/' + pictures[n];
+  element.appendChild(imgBlock);
+
+  document.getElementById('slide-container').appendChild(element);
+}
+
+function updateSlides() {
+  var slides = document.getElementsByClassName('mySlides');
+  while (slides.length != 0 ) {
+    slides[0].parentNode.removeChild(slides[0]);
+  }
+
+  for (var i = 0; i < pictures.length; i++) {
+    addSlide(i);
+  }
+
+  setRange(rangeStart);
+  setSlideInRange(currentSlideInRange);
+}
+
+function changeRange(diff) {
+  var newRangeStart = rangeStart + diff * MAX_SLIDES;
+  setRange(newRangeStart);
+
+  if (diff < 0) {
+    setSlideInRange(MAX_SLIDES - 1);
+  } else {
+    setSlideInRange(0);
+  }
+}
+
+function setRange(rangeNumber) {
+  rangeStart = rangeNumber;
+
+  var moredots = document.getElementsByClassName('moredot');
+  var dots = document.getElementsByClassName('dot');
+  if (rangeStart == 0) {
+    moredots[0].style.display = 'none';
+  } else {
+    moredots[0].style.display = 'inline-block';
+  }
+
+  if (rangeStart + MAX_SLIDES >= pictures.length) {
+    moredots[1].style.display = 'none';
+  } else {
+    moredots[1].style.display = 'inline-block';
+  }
+
+  for (var i = 0; i < MAX_SLIDES; i++) {
+    if (rangeStart + i < pictures.length) {
+      dots[i].style.display = 'inline-block';
+    } else {
+      dots[i].style.display = 'none';
+    }
+  }
+}
+
+function changeSlideInRange(diff) {
+  var slideNumber = currentSlideInRange + diff;
+  var newRangeStart = rangeStart;
+  if (slideNumber < 0) {
+    if (rangeStart - MAX_SLIDES < 0) {
+      return;
+    } else {
+      newRangeStart = rangeStart - MAX_SLIDES;
+      slideNumber = MAX_SLIDES - 1;
+    }
+  } else if (slideNumber >= MAX_SLIDES || rangeStart + slideNumber >= pictures.length) {
+    if (rangeStart + MAX_SLIDES >= pictures.length) {
+      return;
+    } else {
+      newRangeStart = rangeStart + MAX_SLIDES;
+      slideNumber = 0;
+    }
+  }
+
+  if (newRangeStart != rangeStart) {
+    setRange(newRangeStart);
+  }
+  setSlideInRange(slideNumber);
+}
+
+function setSlideInRange(slideNumber) {
+  currentSlideInRange = slideNumber;
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[rangeStart + currentSlideInRange].style.display = "block";
+  dots[currentSlideInRange].className += " active";
+}
+
+// Refresh as pictures come in
+(function worker() {
+  $.ajax({
+    url: 'http://localhost/pictures.json',
+    success: function(data) {
+      pictures = data
+      updateSlides();
+    },
+    complete: function() {
+      // Schedule the next request when the current one's complete
+      setTimeout(worker, 1000);
+    }
+  });
+})();
