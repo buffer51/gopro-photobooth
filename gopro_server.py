de046257c16468d08bfa6826a297cd563a991286
Paul Mustière <pmustiere@palantir.com>
2019-05-26 19:05:59 +0200

    Fix identation

--- gopro_server.py
+++ gopro_server.py
@@ -151,35 +151,35 @@ class MyGoPro:
 
 class GPIOButton():
     def __init__(self, event_hook):
-    self.pressed = False
-    self.last_press = None
-    self.event_hook = event_hook
+        self.pressed = False
+        self.last_press = None
+        self.event_hook = event_hook
 
         GPIO.setmode(GPIO.BOARD)
     	GPIO.setup(CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(CHANNEL, GPIO.BOTH, callback=button.gpio_event)
 
     def gpio_event(self, channel):
-    if GPIO.input(CHANNEL):
-    self.down()
-    else:
-    self.up()
+        if GPIO.input(CHANNEL):
+            self.down()
+        else:
+            self.up()
 
     def down(self):
-    self.pressed = True
+        self.pressed = True
 
     def up(self):
-    if self.pressed:
-    self.maybe_event()
+        if self.pressed:
+            self.maybe_event()
 
-    self.pressed = False
+        self.pressed = False
 
     def maybe_event(self):
-    current_time = time.time()
+        current_time = time.time()
 
-    if not self.last_press or current_time-self.last_press > EVENT_DELAY:
-    self.event_hook()
-    self.last_press = current_time
+        if not self.last_press or current_time-self.last_press > EVENT_DELAY:
+            self.event_hook()
+            self.last_press = current_time
 
 if __name__ == '__main__':
     gopro = MyGoPro()
